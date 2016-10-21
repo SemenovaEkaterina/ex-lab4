88beccd9b56d6637571e608156c6884b3addaabb
kat-semyonova@mail.ru <kat-semyonova@mail.ru>
2016-10-21 17:06:49 +0300

    ex

--- ex_6.py
+++ ex_6.py
@@ -6,20 +6,12 @@ from librip.decorators import print_result
 from librip.gen import field, gen_random
 from librip.iterators import Unique as unique
 
-path = None
+path = sys.argv[1]
 
-# Здесь необходимо в переменную path получить
-# путь до файла, который был передан при запуске
-
-with open("data_light.json") as f:
+with open(path) as f:
     data = json.load(f)
 
 
-# Далее необходимо реализовать все функции по заданию, заменив `raise NotImplemented`
-# Важно!
-# Функции с 1 по 3 дожны быть реализованы в одну строку
-# В реализации функции 4 может быть до 3 строк
-# При этом строки должны быть не длиннее 80 символов
 
 @print_result
 def f1(arg):
@@ -39,7 +31,8 @@ def f3(arg):
 @print_result
 def f4(arg):
     return [x + ", зарплата " + str(y) + " руб." for x, y in zip(arg, gen_random(100000, 200000, len(arg)))]
-	
+
 
 with timer():
     f4(f3(f2(f1(data))))
+
